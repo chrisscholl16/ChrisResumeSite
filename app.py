18e5af779beaf8f8f9e520055b5b92e4c52095ee
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2025-07-12 18:37:45 -0400

    Set up pages nav

--- app.py
+++ app.py
@@ -8,5 +8,21 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/skills')
+def skills():
+    return render_template("skills.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/experience')
+def experience():
+    return render_template("experience.html")
+
 if (__name__) == '__main__':
     app.run(debug=True)
